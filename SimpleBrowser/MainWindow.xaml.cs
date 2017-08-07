6981921b3523ffa790288a8eb0315a8ed4ad5e10
RenaudGrondin <private.renaud.grondin@gmail.com>
2017-08-07 15:45:15 -0400

    changelog
    code documenté et nettoyé
    - Changé le homepage pour duck duck go
    - dorénavant lorsque le url n'est pas valide la requête se transforme en
    recherche duck duck go
    -  dorénavant si un prefixe url n'est pas fourni, https est choisit
    -  réglé bug d'affichage du lien basé sur le contenu chargé dans la
    barre d'adresse
    - réglé bug sur ouverture de lien vers nouvelle tab
    - dorénavant le navigateur démarre au maximum des capacités de l'écran
    -  changé la couleur d'arrière plan et la couleur du label (style GUI)
    - ajouté fonctionalité page precedente, page suivante, bouton acceuil
    (historique de session)
    - icone ajouté
    (https://www.iconfinder.com/icons/1587490/internet_line_icon#size=128)
    licence: gratuit pour utilisation
    commerciale

--- SimpleBrowser/MainWindow.xaml.cs
+++ SimpleBrowser/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ using System.Text.RegularExpressions;
 using System.Diagnostics;
 using Microsoft.Win32;
 using System.Reflection;
+using System.Xml;
+using mshtml;
+using System.IO;
 
 namespace SimpleBrowser
 {
@@ -25,20 +28,39 @@ namespace SimpleBrowser
     /// </summary>
     public partial class MainWindow : Window
     {
-        const string PAGE_DEFAUT = "https://www.whatismybrowser.com/";
+        const string PAGE_DEFAUT = "https://duckduckgo.com/";
+        const string URL_RECHERCHE = "https://duckduckgo.com?q=";
+
+        // utilisé pour référence après chaque transaction
+        private static string _url = String.Empty;
+
+        // historique de session
+        List<string> historique = new List<string>();
+        // position_visite_historiqu
+        int historique_position = 0;
+
+
         public MainWindow()
         {
-            SetBrowserFeatureControl();
+            setBrowserFeatureControl();
             InitializeComponent();          
         }
 
         private void Fenetre_chargee(object sender, RoutedEventArgs e)
         {
-            HideScriptErrors(this.webBrowser, true);
-            this.webBrowser.Navigate(PAGE_DEFAUT);
+            ajusterTailleGUI();
+            mettreAJourButtonsHistorique();
+            hideScriptErrors(this.webBrowser, true);
+            naviguer(this.webBrowser, PAGE_DEFAUT);
+            this.historique.Add(PAGE_DEFAUT);
             textbox_url.Text = PAGE_DEFAUT;
         }
 
+        /// <summary>
+        /// verifie si lien est valide
+        /// </summary>
+        /// <param name="url">lien</param>
+        /// <returns>la reponse</returns>
         private bool checkUrl(string url)
         {          
             if(Uri.IsWellFormedUriString(url, UriKind.Absolute))
@@ -69,39 +91,52 @@ namespace SimpleBrowser
             
         }
 
-      
+        
         private void btn_aller_Click(object sender, RoutedEventArgs e)
         {
-            if (!((textbox_url.Text).StartsWith("http://") && !((textbox_url.Text).StartsWith("https://"))))
+            var url = this.textbox_url.Text;
+
+            var isPrefixHTTP = (url).StartsWith("http://");
+            var isPrefixHTTPS = (url).StartsWith("https://");
+
+            if(isPrefixHTTP == false && isPrefixHTTPS == isPrefixHTTP)
             {
-                (textbox_url.Text) = "http://" + (textbox_url.Text);
+                // si le url ne possde pas de prefixe, ajouter prefixe
+                url = "https://" + (url);
+                this.textbox_url.Text = url;
             }
 
-            if (checkUrl(textbox_url.Text))
-            {
-                this.webBrowser.Navigate(textbox_url.Text);
+            if (checkUrl(url))
+            {   // si le url est valide, naviguer vers url
+                naviguer(this.webBrowser, url);
+
+                // ajouter a historique
+                this.historique.Add(url);
+                // mettre  a jour la position par rapport a l'historique
+                this.historique_position = this.historique.Count() - 1;
             }
+            else
+            {   // faire recherche avec le lien fautif pour permettre une suggestion
+                naviguer(this.webBrowser, URL_RECHERCHE + textbox_url.Text);
+            }
+            mettreAJourButtonsHistorique();
         }
 
+        /// <summary>
+        /// alias, appelle btn_aller_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void textbox_url_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Return)
             {
-                if (!((textbox_url.Text).StartsWith("http://") && !((textbox_url.Text).StartsWith("https://"))))
-                {
-                    (textbox_url.Text) = "http://" + (textbox_url.Text);
-                    if (checkUrl(textbox_url.Text))
-                    {
-                        this.webBrowser.Navigate(textbox_url.Text);
-                    }
-                }
-    
+                this.btn_aller_Click(sender, e);
             }
         }
 
-
         // charger temporairement les parametres
-        private void SetBrowserFeatureControlKey(string feature, string appName, uint value)
+        private void setBrowserFeatureControlKey(string feature, string appName, uint value)
         {
             using (var key = Registry.CurrentUser.CreateSubKey(
                 String.Concat(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\", feature),
@@ -110,42 +145,44 @@ namespace SimpleBrowser
                 key.SetValue(appName, (UInt32)value, RegistryValueKind.DWord);
             }
         }
-
-
-        private void SetBrowserFeatureControl()
+        
+        // initialisation des parametres pour webbrowser
+        private void setBrowserFeatureControl()
         {
             // http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
 
             // FeatureControl settings are per-process
             var fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
 
-            SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, GetBrowserEmulationMode()); // Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
-            SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1);
-            SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0);
-            SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, getBrowserEmulationMode()); // Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
+            setBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1);
+            setBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0);
+            setBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1);
         }
 
-        private UInt32 GetBrowserEmulationMode()
+        // https://stackoverflow.com/a/28626667
+        // en lien avec setBrowserFeatureControl
+        private UInt32 getBrowserEmulationMode()
         {
             int browserVersion = 7;
             using (var ieKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer",
@@ -185,8 +222,9 @@ namespace SimpleBrowser
             return mode;
         }
 
-        // cacher les messages derreurs javascript
-        public void HideScriptErrors(WebBrowser wb, bool Hide)
+        // https://stackoverflow.com/a/24377136
+        // cacher les messages derreurs javascript (bugifx)
+        private void hideScriptErrors(WebBrowser wb, bool Hide)
         {
             FieldInfo fiComWebBrowser = typeof(WebBrowser)
                 .GetField("_axIWebBrowser2",
@@ -199,6 +237,101 @@ namespace SimpleBrowser
                 new object[] { Hide });
         }
 
+        private void naviguer(WebBrowser wb, string url)
+        {
+            wb.Navigate(url);
+            textbox_url.Text = url;
+            _url = url;
+        }
+
+        // https://stackoverflow.com/a/8005898
+        // https://stackoverflow.com/a/6222430
+        // effectuer des changements dans le dom du navigateur
+        private void webBrowser_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            var wb = sender as WebBrowser;
+            var document = wb.Document as mshtml.HTMLDocument;
+            string path = Environment.CurrentDirectory + @"\injectedJavascript.js"; 
+
+            mshtml.IHTMLDocument2 htmldoc = this.webBrowser.Document as IHTMLDocument2;
+            IHTMLElement el = (IHTMLElement)htmldoc.createElement("script");
+            IHTMLScriptElement scriptEl = (IHTMLScriptElement)el;
+            scriptEl.text = File.ReadAllText(path);
+
+            el.id = "injectedScript";
+
+            IHTMLDOMNode node = (IHTMLDOMNode)el;
+            mshtml.HTMLBodyClass body = htmldoc.body as mshtml.HTMLBodyClass;
+            body.appendChild(node);
+
+            // mettre a jour la barre d'adresse
+            textbox_url.Text = document.location.toString();
+
+            // debugging
+            //document.body.innerText = document.body.innerHTML.ToString();
+
+        }
+
+        /// <summary>
+        /// ajuste les elements du GUI pour une taille optimale par rapport a l'ecran
+        /// </summary>
+        private void ajusterTailleGUI()
+        {
+            int width = int.Parse(System.Windows.SystemParameters.PrimaryScreenWidth.ToString());
+            int height = int.Parse(System.Windows.SystemParameters.PrimaryScreenHeight.ToString());
+
+            // stretching
+            Application.Current.MainWindow.Width = width;
+            Application.Current.MainWindow.Height = height;
+
+            this.webBrowser.Height = (height * 0.92);
+            this.webBrowser.Width = (width * 0.99);
+        }
+
+        // fonctionalite pour historique, btn precendent
+        private void btn_historique_precedent_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.historique_position > 0)
+            {
+                this.historique_position -= 1;
+                naviguer(this.webBrowser, this.historique[this.historique_position]);
+            }
+            mettreAJourButtonsHistorique();
+        }
+
+        // fonctionalite pour historique, btn suivant
+        private void btn_historique_suivant_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.historique_position + 1 < this.historique.Count())
+            {
+                this.historique_position += 1;
+                naviguer(this.webBrowser, this.historique[this.historique_position]);
+            }
+            mettreAJourButtonsHistorique();
+        }
+
+        // fonctionalite bouton page acceuil
+        private void btn_accueil_Click(object sender, RoutedEventArgs e)
+        {
+            naviguer(this.webBrowser, PAGE_DEFAUT);
+        }
+
+        // mettre a jour ui pour historique
+        private void mettreAJourButtonsHistorique()
+        {
+            // la premiere fois
+            if(this.historique.Count() == 0)
+            {
+                this.btn_historique_precedent.Visibility = Visibility.Hidden;
+                this.btn_historique_suivant.Visibility = Visibility.Hidden;
+            }else
+            { // les autres fois
+                this.btn_historique_suivant.Visibility = Visibility.Visible;
+                this.btn_historique_precedent.Visibility = Visibility.Visible;
+            }
+
+           
+        }
 
     }
 }
---
+++ SimpleBrowser/injectedJavascript.js
@@ -0,0 +1,2 @@
+﻿var body = document.getElementsByTagName('body')[0];
+body.innerHTML += "<h1 id='poc' style='position: absolute;top:0;right: 0;z-index: 1000;'>Simple Browser</h1>";
